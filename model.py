d801bf164d289ab37ec5408250374268a3951d86
yuan <sjtuliuyuan@gmail.com>
2018-01-14 12:25:57 +0800

    change weight initilizer to xaiver

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 
 # Returns tuple of flat output, flat output size, network_parameters.
 def create_conv_network(input_frames, trainable):
-    conv1_W = tf.Variable(tf.random_normal([8, 8, 4, 16], stddev=0.1, dtype=tf.float32),
-        name='conv1_W', trainable=trainable)
+    conv1_W = tf.get_variable(shape=[8, 8, 4, 16], name='conv1_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     conv1_b = tf.Variable(tf.zeros([16], dtype=tf.float32),
         name='conv1_b', trainable=trainable)
     conv1 = tf.nn.conv2d(input_frames, conv1_W, strides=[1, 4, 4, 1],
@@ -13,8 +13,8 @@ def create_conv_network(input_frames, trainable):
     # (batch size, 20, 20, 16)
     output1 = tf.nn.relu(conv1 + conv1_b, name='output1')
 
-    conv2_W = tf.Variable(tf.random_normal([4, 4, 16, 32], stddev=0.1, dtype=tf.float32),
-        name='conv2_W', trainable=trainable)
+    conv2_W = tf.get_variable(shape=[4, 4, 16, 32], name='conv2_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     conv2_b = tf.Variable(tf.zeros([32], dtype=tf.float32), name='conv2_b',
         dtype=tf.float32, trainable=trainable)
     conv2 = tf.nn.conv2d(output1, conv2_W, strides=[1, 2, 2, 1],
@@ -31,15 +31,15 @@ def create_conv_network(input_frames, trainable):
 # Returns tuple of network, network_parameters.
 def create_deep_q_network(input_frames, input_length, num_actions, trainable):
     flat_output, flat_output_size, parameter_list = create_conv_network(input_frames, trainable)
-    fc1_W = tf.Variable(tf.random_normal([flat_output_size, 256], stddev=0.1, dtype=tf.float32),
-        name='fc1_W', trainable=trainable)
+    fc1_W = tf.get_variable(shape=[flat_output_size, 256], name='fc1_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fc1_b = tf.Variable(tf.zeros([256], dtype=tf.float32), name='fc1_b',
         trainable=trainable)
     # (batch size, 256)
     output3 = tf.nn.relu(tf.matmul(flat_output, fc1_W) + fc1_b, name='output3')
 
-    fc2_W = tf.Variable(tf.random_normal([256, num_actions], stddev=0.1, dtype=tf.float32),
-        name='fc2_W', trainable=trainable)
+    fc2_W = tf.get_variable(shape=[256, num_actions], name='fc2_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fc2_b = tf.Variable(tf.zeros([num_actions], dtype=tf.float32), name='fc2_b',
         trainable=trainable)
     # (batch size, num_actions)
@@ -53,27 +53,27 @@ def create_deep_q_network(input_frames, input_length, num_actions, trainable):
 def create_duel_q_network(input_frames, input_length, num_actions, trainable):
     flat_output, flat_output_size, parameter_list = create_conv_network(input_frames, trainable)
 
-    fcV_W = tf.Variable(tf.random_normal([flat_output_size, 512], stddev=0.01, dtype=tf.float32),
-        name='fcV_W', trainable=trainable)
+    fcV_W = tf.get_variable(shape=[flat_output_size, 512], name='fcV_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fcV_b = tf.Variable(tf.zeros([512], dtype=tf.float32), name='fcV_b',
         dtype=tf.float32, trainable=trainable)
     outputV = tf.nn.relu(tf.matmul(flat_output, fcV_W) + fcV_b, name='outputV')
 
-    fcV2_W = tf.Variable(tf.random_normal([512, 1], stddev=0.01, dtype=tf.float32),
-        name='fcV2_W', trainable=trainable)
+    fcV2_W = tf.get_variable(shape=[512, 1], name='fcV2_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fcV2_b = tf.Variable(tf.zeros([1], dtype=tf.float32), name='fcV2_b',
         trainable=trainable)
     outputV2 = tf.nn.relu(tf.matmul(outputV, fcV2_W) + fcV2_b, name='outputV2')
 
 
-    fcA_W = tf.Variable(tf.random_normal([flat_output_size, 512], stddev=0.01, dtype=tf.float32),
-        name='fcA_W', trainable=trainable)
+    fcA_W = tf.get_variable(shape=[flat_output_size, 512], name='fcA_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fcA_b = tf.Variable(tf.zeros([512], dtype=tf.float32), name='fcA_b',
         trainable=trainable)
     outputA = tf.nn.relu(tf.matmul(flat_output, fcA_W) + fcA_b, name='outputA')
 
-    fcA2_W = tf.Variable(tf.random_normal([512, num_actions], stddev=0.01, dtype=tf.float32),
-        name='fcA2_W', trainable=trainable)
+    fcA2_W = tf.get_variable(shape=[512, num_actions], name='fcA2_W',
+        trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     fcA2_b = tf.Variable(tf.zeros([num_actions], dtype=tf.float32), name='fcA2_b',
         trainable=trainable)
     outputA2 = tf.nn.relu(tf.matmul(outputA, fcA2_W) + fcA2_b, name='outputA2')
@@ -86,7 +86,7 @@ def create_duel_q_network(input_frames, input_length, num_actions, trainable):
 
 def create_model(window, input_shape, num_actions, model_name, create_network_fn, trainable):
     """Create the Q-network model."""
-    with tf.name_scope(model_name):
+    with tf.variable_scope(model_name):
         input_frames = tf.placeholder(tf.float32, [None, input_shape[0],
                         input_shape[1], window], name ='input_frames')
         input_length = input_shape[0] * input_shape[1] * window
@@ -113,7 +113,7 @@ def create_distributional_model(window, input_shape, num_actions, model_name, cr
     z_list_broadcasted = tf.tile(tf.reshape(z_list,[1,N_atoms]), tf.constant([num_actions,1]))
 
     """Create the Q-network model."""
-    with tf.name_scope(model_name):
+    with tf.variable_scope(model_name):
         input_frames = tf.placeholder(tf.float32, [None, input_shape[0],
                         input_shape[1], window], name ='input_frames')
         input_length = input_shape[0] * input_shape[1] * window
